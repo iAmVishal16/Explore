3447926499c077030509eea53340f1f8bc3afc01
Vishal Paliwal <vishal.paliwal@ril.com>
2023-05-15 22:22:19 +0530

    Initial Commit

---
+++ Explore/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Explore
+//
+//  Created by Vishal Paliwal on 15/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Explore/ExploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreApp.swift
+//  Explore
+//
+//  Created by Vishal Paliwal on 15/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ExploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
